ec4d30b3a9a27fc95cfda2bd55ae08dcb38eb872
MalvadoPerro <diman_1205@mail.ru>
2024-07-07 23:58:49 +0300

    upd

---
+++ jdrm/main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.template.defaulttags import register
+from django.http import HttpResponse
+from datetime import date
+
+@register.filter
+def get_name_url(path):
+    return path.replace('/', '')
+
+def main(request):
+    year, month, day = str(date.today()).split('-')
+    year = int(year)
+    month = int(month)
+    day = int(day)
+    months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 
+              'Октябрь', 'Ноябрь', 'Декабрь']
+
+    data = {
+        'year': year,
+        'month': months[month - 1],
+        'day': day
+    }
+    return render(request, 'main/main.html', data)
---
+++ test_template/register.php
@@ -0,0 +1,7 @@
+<?php
+
+$login = $_POST['login'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$repeat = $_POST['repeatPassword'];
+
